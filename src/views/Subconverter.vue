3069caacccebf83b77d60c7c5efb82d143d9cef9
DurganShelley <67219840+DurganShelley@users.noreply.github.com>
2021-10-09 20:40:30 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -351,7 +351,7 @@ export default {
                   "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/special/basic.ini"
               }
             ]
-          }          
+          }
         ]
       },
       form: {
